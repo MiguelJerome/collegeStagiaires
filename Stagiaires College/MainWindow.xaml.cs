ed21e06abf2f04c1d1994eb4fac270d263c635ac
MiguelJerome <sharkmaycry@gmail.com>
2022-11-19 12:30:33 -0500

    add list program stagiaires

--- Stagiaires College/MainWindow.xaml.cs
+++ Stagiaires College/MainWindow.xaml.cs
@@ -15,19 +15,90 @@ using System.Windows.Shapes;
 
 namespace Stagiaires_College
 {
+    using System.Collections.ObjectModel;
+    using System.Runtime.CompilerServices;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class Programme
+    {
+        public int id { get; set; }
+
+        public string nom { get; set; }
+
+        public int dureeEnMois { get; set; }
+
+        public Programme(int id, string nom, int dureeEnMois)
+        {
+            this.id = id; // 7 chiffres only
+            this.nom = nom; // testing programme name
+            this.dureeEnMois = dureeEnMois; // minimun 1 , maxinmum 60 mois
+        }
+
+        public override string ToString()
+        {
+            return $"{this.id} {this.nom} {this.dureeEnMois}";
+        }
+    }
+
+    public class Stagiaire
+    {
+        public int id { get; set; } // 7 chiffres only
+
+        public string nom { get; set; } // string letters only 
+
+        public string prenom { get; set; } // string letters only
+
+        public bool sexe { get; set; } // only female, only male
+        //          public int programme_id { get; set; }
+
+        public Stagiaire(int id, string nom, string prenom)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.prenom = prenom;
+         //   this.sexe = sexe;
+            //     this.programme_id = programme_id;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{this.id} {this.nom} {this.prenom} {this.sexe}";
+        }
+
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Programme> programmes = new ObservableCollection<Programme>();
+
+        private ObservableCollection<Stagiaire> stagiaires = new ObservableCollection<Stagiaire>();
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void ID_Programmes_Copy_TextChanged(object sender, TextChangedEventArgs e)
-        {
+            programmes.Add(new Programme(2001, "Math", 60));
+            programmes.Add(new Programme(2001, "English", 60));
+            programmes.Add(new Programme(2001, "Spanish", 60));
+            stagiaires.Add(new Stagiaire(2001111, "Papa", "Mama"));
+            stagiaires.Add(new Stagiaire(2001111, "sister", "brother"));
+
+            this.ListeViewProgrammes.ItemsSource = programmes;
+            this.ListeViewStagiaires.ItemsSource = stagiaires;
+
+            this.ListViewProgrammeConsulter.ItemsSource = stagiaires;
+
 
         }
     }
+
+
+
+
+
+
 }
